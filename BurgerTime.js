9da8d7601d27f3b173bfa2b9d54172f4332d54d4
Mirzafar <mirzafar546@gmail.mail>
2023-02-23 23:36:14 +0500

    imtihon 3-oy

--- BurgerTime.js
+++ BurgerTime.js
@@ -1,38 +1,101 @@
-let buy = document.querySelector(".Buy");
+let total = document.querySelector(".total");
 let Cheese = document.querySelector(".Cheese");
 let Meat = document.querySelector(".Meat");
 let Onion = document.querySelector(".Onion");
 let Salad = document.querySelector(".Salad");
 let Tomato = document.querySelector(".Tomato");
 let Pickle = document.querySelector(".Pickle");
-let total = document.querySelector(".total");
-let im  = document.createElement('img')
-im.classList.add('ig')
-let img = document.querySelector('.ig')
+let bottom = document.querySelector("#bottom");
+let Buy = document.querySelector(".Buy");
 
-Cheese.addEventListener("click" , () => {
-         total.textContent =  +total.textContent + 2
-           im.setAttribute("src" , "./BurgerTime/cheese.png ");
+Buy.addEventListener("click" , () => {
+  alert(`Xaridingiz uchun rahmat sizning ochuritngiz${Math.floor(Math.random() * (35 - 10) + 10 )}`)
 })
-Meat.addEventListener("click" , () => {
-         total.textContent = +total.textContent + 5
-})
+
+Cheese.addEventListener("click", () => {
+  total.textContent = +total.textContent + 2;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li")
+  let img = document.createElement("img");
+  img.classList.add("immg")
+  img.setAttribute("src", "./BurgerTime/cheese.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li , bottomB )
+  li.prepend(img)
+  img.addEventListener("click", () =>{
+    img.classList.add("none")
+    total.textContent = +total.textContent + -2;
+
+  } )
+});
+Meat.addEventListener("click", () => {
+  total.textContent = +total.textContent + 5;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li")
+  let img = document.createElement("img");
+  img.setAttribute("src", "./BurgerTime/meat.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li , bottomB )
+  li.prepend(img)
+  img.addEventListener("click", () => {
+    img.classList.add("none");
+    total.textContent = +total.textContent + -5;
+  });
+});
 Onion.addEventListener("click", () => {
   total.textContent = +total.textContent + 1;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li")
+  let img = document.createElement("img");
+  img.setAttribute("src", "./BurgerTime/onion.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li , bottomB )
+  li.prepend(img)
+  img.addEventListener("click", () => {
+    img.classList.add("none");
+    total.textContent = +total.textContent + -1;
+  });
 });
 Salad.addEventListener("click", () => {
   total.textContent = +total.textContent + 1;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  let img = document.createElement("img");
+  img.setAttribute("src", "./BurgerTime/salad.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li, bottomB);
+  li.prepend(img);
+  img.addEventListener("click", () => {
+    img.classList.add("none");
+    total.textContent = +total.textContent + -1;
+  });
 });
 Tomato.addEventListener("click", () => {
   total.textContent = +total.textContent + 2;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  let img = document.createElement("img");
+  img.setAttribute("src", "./BurgerTime/tomato.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li, bottomB);
+  li.prepend(img);
+  img.addEventListener("click", () => {
+    img.classList.add("none");
+    total.textContent = +total.textContent + -2;
+  });
 });
 Pickle.addEventListener("click", () => {
   total.textContent = +total.textContent + 3;
+  let ul = document.querySelector("ul");
+  let li = document.createElement("li");
+  let img = document.createElement("img");
+  img.setAttribute("src", "./BurgerTime/pickle.png");
+  let bottomB = document.querySelector("#bottom");
+  ul.insertBefore(li, bottomB);
+  li.prepend(img);
+  img.addEventListener("click", () => {
+    img.classList.add("none");
+    total.textContent = +total.textContent + -3;
+  });
 });
 
-
-buy.addEventListener("click", () => {
-    let a = Math.round(Math.random() * (35 - 10) + 10);
-    alert(`To'lo'v Amalga Oshirildi Sizning Ochurtingiz ${a} `);
-
-});
